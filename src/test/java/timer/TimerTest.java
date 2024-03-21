ae1318201e894edf898471f38072a92f27310434
laurianekoalaga <93921732+laurianekoalaga@users.noreply.github.com>
2024-03-21 12:09:02 +0100

    test BB Timer

--- src/test/java/timer/TimerTest.java
+++ src/test/java/timer/TimerTest.java
@@ -2,6 +2,26 @@ package timer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TimerTest {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+public class TimerTest implements Timer {
+    private Iterator<Integer> iterator;
+
+    public TimerTest(Iterable<Integer> values) {
+        this.iterator = values.iterator();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return iterator.next();
+    }
 }
